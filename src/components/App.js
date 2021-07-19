9ef7fb04892ec79bc8b0da25dc548c83d02f34c2
vincentbaylon <vincentbaylon@gmail.com>
2021-07-19 09:07:28 -0700

    commit tree ec1ebc9e679c6366bd3249999e0a56431310877b

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import NavBar from "NavBar.js"
-import Home from "Home.js"
-import About from "About.js"
+import NavBar from "./NavBar.js"
+import Home from "./Home.js"
+import About from "./About.js"
 
 function App() {
   return (
